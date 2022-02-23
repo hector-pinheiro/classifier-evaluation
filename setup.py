91c2c5e15fd7fdc53d2c69552ae55004cedc3d0f
Hector Pinheiro <hector.pinheiro@incognia.com>
2022-02-23 11:42:29 -0300

    chore(version): add version file and update version to 0.2.0

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open('VERSION') as f:
+    version = f.read()
+
 setuptools.setup(
     name='modeleval',
-    version='0.0.1',
+    version=version,
     description='Evaluation of mlflow binary classifiers',
     long_description=long_description,
     long_description_content_type="text/markdown",
